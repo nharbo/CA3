e9b22005a49b85930e9cc96967d870191c0c270b
nharbo <nicolaiharbo@10.50.137.175>
2015-11-03 11:16:07 +0100

    view 5 og 6 fix efter git-fuckup

--- AngSeedClient/app/view2/view2.js
+++ AngSeedClient/app/view2/view2.js
@@ -4,19 +4,7 @@ angular.module('myApp.view2', ['ngRoute'])
 
         .config(['$routeProvider', function ($routeProvider) {
             $routeProvider.when('/view2', {
-              templateUrl: 'view2/view2.html',
-              controller: 'View2Ctrl'
+              templateUrl: 'view2/view2.html'
             });
-          }])
+          }]);
 
-        .controller('View2Ctrl', function ($http, $scope) {
-          $http({
-            method: 'GET',
-            url: 'api/demouser'
-          }).then(function successCallback(res) {
-            $scope.data = res.data.message;
-          }, function errorCallback(res) {
-            $scope.error = res.status + ": "+ res.data.statusText;
-          });
-
-        });
---
+++ AngSeedClient/app/view6/view6.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('myApp.view6', ['ngRoute'])
+
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view6', {
+                    templateUrl: 'view6/view6.html'
+                });
+            }]);
