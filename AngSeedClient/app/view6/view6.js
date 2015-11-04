b4bb8b062307b4071ec8f9dda6e0f6846322122a
Daniel <Daniel@10.50.138.20>
2015-11-04 09:59:45 +0100

    view6 - oprettelse af $http post method og tilhørende tilbagemeldinger ved success eller fejl.

--- AngSeedClient/app/view6/view6.js
+++ AngSeedClient/app/view6/view6.js
@@ -6,4 +6,21 @@ angular.module('myApp.view6', ['ngRoute'])
                 $routeProvider.when('/view6', {
                     templateUrl: 'view6/view6.html'
                 });
-            }]);
+            }])
+
+        .controller('View6Ctrl', ['$http', '$scope', function ($http, $scope) {
+
+//                var createUserObj = {
+//                    username: $scope.usernameInput,
+//                    password: $scope.passwordInput
+//                };
+
+                $http({
+                    method: 'POST',
+                    url: 'http://localhost:8081/AngSeedServer/api/demouser/create' + "{" + $scope.usernameInput + "}" + "{" + $scope.passwordInput + "}",
+                }).then(function successCallback(response) {
+                    $scope.returnMessage = response;
+                }, function errorCallback(response) {
+                    alert("Failure message: " + JSON.stringify({reponse: response}))
+                });
+            }])
