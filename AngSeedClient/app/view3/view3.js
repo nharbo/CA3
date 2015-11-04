8777a81dcbb7fdaa65408de8b7e0515650fc2d39
nharbo <nicolaiharbo@10.50.137.175>
2015-11-04 10:57:36 +0100

    view 3 virker nu som den skal - der kan søges og resultatet vises som det skal! job done! Der skal muligvis redigeres lidt i udseenet af siden ;-)

--- AngSeedClient/app/view3/view3.js
+++ AngSeedClient/app/view3/view3.js
@@ -9,36 +9,68 @@ angular.module('myApp.view3', ['ngRoute'])
                 });
             }])
 
-        .controller('View3Ctrl', ["MyFactory", function (MyFactory) {
+        .controller('View3Ctrl', function ($http) {
 
             var self = this;
-
-            self.company = function (input) {
-                return MyFactory.getCompany(input);
-            };
+//            self.info = {name: "test"};
+//            self.test = "test!!";
 
 
-        }])
-
-        .factory('MyFactory', function ($http) {
-
-            var getCompany = function (input) {
-                
-                var country = input;
+            self.searchbutton = function () {
+//                alert("in function");
+                var searchfield = self.searchfield;
+                var option = self.option;
+                var country = self.country;
+                console.log(option);
+                console.log(searchfield);
                 console.log(country);
 
-                $http.get('http://localhost:8080/AngSeedServer/api/demouser/getcomp/name/lego/dk')
-//                $http.get('http://localhost:8080/AngSeedServer/api/demouser/getcomp/' + input.option + '/' + input.name + '/' + input.country)
+
+                $http.get('http://localhost:8080/AngSeedServer/api/demouser/getcomp/' + option + '/' + searchfield + '/' + country)
                         .success(function (response) {
+//                            alert("success!");
                             console.log(response);
+                            console.log(response.name);
+                            self.info = response;
                         })
                         .error(function (response) {
-
+//                            alert("Error!1!");
+                            self.errormessage = "Fejl i søgningen - prøve igen!";
                         });
-            };
-            
-            return{
-                getCompany: getCompany
+
+                self.company = function (input) {
+                    MyFactory.getCompany(input);
+                };
             }
 
+
+
+
         });
+
+//        .factory('MyFactory', function ($http) {
+//
+//            var getCompany = function (input) {
+//
+//                var country = input;
+//                console.log(country);
+//
+//                $http.get('http://localhost:8080/AngSeedServer/api/demouser/getcomp/name/lego/dk')
+////                $http.get('http://localhost:8080/AngSeedServer/api/demouser/getcomp/' + input.option + '/' + input.name + '/' + input.country)
+//                        .success(function (response) {
+//                            alert("her!");
+//                            console.log(response);
+//                            
+//                        })
+//                        .error(function (response) {
+//                            alert("Error!1!");
+//                        });
+//
+//
+//            };
+//
+//            return{
+//                getCompany: getCompany
+//            };
+//
+//        });
