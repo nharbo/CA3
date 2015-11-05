2b0fe340a9574fc76a3029b6e9053d5657cec95e
Daniel <Daniel@10.50.138.20>
2015-11-05 14:24:40 +0100

    Frontend ændringer

--- AngSeedClient/app/view4/view4.js
+++ AngSeedClient/app/view4/view4.js
@@ -2,12 +2,12 @@
 
 angular.module('myApp.view4', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view4', {
-    templateUrl: 'view4/view4.html',
-    controller: 'View4Ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view4', {
+                    templateUrl: 'view4/view4.html',
+                    controller: 'View4Ctrl'
+                });
+            }])
 
         .controller('View4Ctrl', function ($http, $scope) {
             $http.get('api/demouser')
@@ -17,5 +17,33 @@ angular.module('myApp.view4', ['ngRoute'])
                     .error(function (data, status, headers, config) {
 
                     });
+
+            // $scope.currencies = {};
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8080/AngSeedServer/api/Currency',
+                cache: true
+            }).success(function (response) {
+                console.log(response);
+                $scope.currencies = response;
+//                for (var i = 0; i < $scope.currencies.length; i++) {
+//                    if ($scope.currencies[i].code === "EUR") {
+//                        $scope.fromCurrency = $scope.currencies[i].code;
+//                    }
+//                    if ($scope.currencies[i].code === "USD") {
+//                        $scope.toCurrency = $scope.currencies[i].code;
+//                    }
+//                }
+            }).error(function () {
+                console.log("Converting failure");
+            });
+
+            $scope.calcConvert = function () {
+                var from = $scope.fromCurrency;
+                var to = $scope.toCurrency;
+                var input = $scope.fromCurrencyVal;
+                $scope.toCurrencyVal = input * (from/to);
+            }
 
         });
