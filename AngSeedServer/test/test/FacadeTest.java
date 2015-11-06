1491416f2966e8f2e6f5d9997cae7e398c72aa93
Florent <Florent@10.50.137.153>
2015-11-06 12:28:22 +0100

    API Test færdig, igang med JUNIT af facade

--- AngSeedServer/test/test/FacadeTest.java
+++ AngSeedServer/test/test/FacadeTest.java
@@ -5,6 +5,8 @@
  */
 package test;
 
+import facades.UserFacade;
+import static org.hamcrest.Matchers.is;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -29,18 +31,34 @@ public class FacadeTest {
     
     @BeforeClass
     public static void setUpClass() {
+       
     }
     
     @AfterClass
     public static void tearDownClass() {
+       
     }
     
     @Before
     public void setUp() {
+         facades.UserFacade.createUser("deleteTest", "delete");
     }
     
     @After
     public void tearDown() {
+        // facades.UserFacade.deleteUser("deleteTest");
+    }
+    
+    @Test
+    public void TestFindUserRoleById(){
+        assertEquals("Admin", facades.UserFacade.getUserByUserId("admin").getRoles().get(0));
+    }
+    
+    @Test
+    public void DeleteUserFromDatabase(){
+        assertEquals("deleteTest", facades.UserFacade.getUserByUserId("deleteTest").getUserName());
+        facades.UserFacade.deleteUser("deleteTest");
+        assertEquals("noUserFound", facades.UserFacade.getUserByUserId("deleteTest").getUserName());
     }
     
 }
